62c8543b6f583384025e8bccab6cd41051c23712
Parv Pratap Singh <parvjadaun7@gmail.com>
2026-06-06 23:41:07 +0530

    Added Day5 programs

---
+++ day5/question20.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,i,largest=1;
+
+    printf("enter a number:");
+    scanf("%d",&n);
+    for(i=2;i<=n;i++){
+        while(n%i==0){
+            largest=i;
+            n=n/i;
+        }
+    }
+    printf("largest prime factor=%d",largest);
+    return 0;
+}
